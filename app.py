912940661b5a1a9c0783c48a2214c5881eb47640
jusolif <juliosolis77@gmail.com>
2026-09-09 20:03:01 -0500

    Replace Python logo image with updated version

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ modulo = st.sidebar.selectbox("Seleccione una sección:",["Home","Carga del data
 if modulo == "Home":
     st.title("Primer Proyecto de Portafolio Profesional")
     st.image("DMC.png",width=150)
-    st.image("Python_logo.png",width=300)
+    st.image("Python.png",width=300)
     st.subheader("Julio Humberto Solis Flores")
     st.markdown("Especialización en Python for Analytics")
     st.write("2026")
